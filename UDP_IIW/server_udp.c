90a082c162737cc05e643b4cff67ef569960cc16
Alessandro Ferraioli <ferrass95@live.it>
2018-03-15 10:06:29 +0100

    change names
    Cambiati nomi header in server_udp.c

--- UDP_IIW/server_udp.c
+++ UDP_IIW/server_udp.c
@@ -95,14 +95,14 @@ Process_data* get_shared_memory(int mem_id)
 
 
 
-void listen_request(int sockfd,Header* p,struct sockaddr_in* addr,socklen_t* len)
+void listen_request(int sockfd,Ptk_head* p,struct sockaddr_in* addr,socklen_t* len)
 {
     struct sockaddr_in servaddr = *addr;
     socklen_t l = *len;
     l = sizeof(servaddr);
     printf("listening request\n");
 
-    if((recvfrom(sockfd, p, sizeof(Header), 0, (struct sockaddr *)&servaddr, &l)) < 0)
+    if((recvfrom(sockfd, p, sizeof(Ptk_head), 0, (struct sockaddr *)&servaddr, &l)) < 0)
          err_exit("recvfrom\n");
 
     *addr = servaddr;
@@ -115,13 +115,13 @@ void listen_request(int sockfd,Header* p,struct sockaddr_in* addr,socklen_t* len
 
 
 
-void send_file_server(char comm[],int sockfd,Header p,struct sockaddr_in servaddr)
+void send_file_server(char comm[],int sockfd,Ptk_head p,struct sockaddr_in servaddr)
 {
 	int n_ack_received = 0,next_ack,i = 0;
 	int fd = -1;
 	struct thread_data td;
 	int end_seq = 0,win_ind,start_seq = p.n_seq;
-	Header recv_h;
+	Ptk_head recv_h;
 	Window* w = NULL;
 	struct timespec arrived;
 
@@ -294,7 +294,7 @@ void send_file_server(char comm[],int sockfd,Header p,struct sockaddr_in servadd
 
 
 
-void get_file_server(char comm[],int sockfd,Header p,struct sockaddr_in servaddr)
+void get_file_server(char comm[],int sockfd,Ptk_head p,struct sockaddr_in servaddr)
 {
 	int fd, i=0, n_pkt,start_seq,win_ind,expected_ack;
 	int existing = 0;
@@ -325,7 +325,7 @@ void get_file_server(char comm[],int sockfd,Header p,struct sockaddr_in servaddr
 
 	send_ack(sockfd,p,servaddr,COMMAND_LOSS,p.n_seq);							/*send ack for command*/
 
-	Header recv;
+	Ptk_head recv;
 
 	/*server waiting for size file; if not received, tries 10 times to resend it*/
 	if(!receive_ack(w,sockfd,servaddr,&recv,p.n_seq + 1,'r',existing)){
@@ -402,7 +402,7 @@ void get_file_server(char comm[],int sockfd,Header p,struct sockaddr_in servaddr
 ****************************************************************/
 void manage_client(int sockfd,struct msgbuf msg)
 {
-	Header r;
+	Ptk_head r;
 	struct sockaddr_in servaddr;
 
     char comm[15];
@@ -463,7 +463,7 @@ void child_job(int qid,int sid,pid_t pid)
 
 	int n_req = 0;
 	(void)pid;
-	Header p;
+	Ptk_head p;
 	struct sockaddr_in addr;
 	int sockfd;
 	struct msgbuf msg;
@@ -492,7 +492,7 @@ void child_job(int qid,int sid,pid_t pid)
 		p.n_seq = -1;
 		p.n_ack = msg.client_seq;
 
-		if (sendto(sockfd, &p, sizeof(Header), 0, (struct sockaddr *)&msg.s, sizeof(msg.s)) < 0)			/*connection ack*/
+		if (sendto(sockfd, &p, sizeof(Ptk_head), 0, (struct sockaddr *)&msg.s, sizeof(msg.s)) < 0)			/*connection ack*/
 			err_exit("sendto");
 
 		manage_client(sockfd,msg);						//execute client request
@@ -533,7 +533,7 @@ void initialize_processes(int qid,int sid)
 
 
 
-void write_on_queue(int qid,struct sockaddr_in s,Header p)
+void write_on_queue(int qid,struct sockaddr_in s,Ptk_head p)
 {
 
 	 struct msgbuf msg;
@@ -617,7 +617,7 @@ int main(int argc, char **argv)
   int sid;
   socklen_t len;
   struct sockaddr_in addr;
-  Header p;
+  Ptk_head p;
   struct sigaction sa;
 
 
